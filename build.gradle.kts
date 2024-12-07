bb74a6082d6a8aeb5cf7f2572b740cde5d69a0bb
Samer04-debug <clomasamuel12@gmail.com>
2024-12-07 22:06:14 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 buildscript {
@@ -10,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2") // Ensure you have the latest AGP version
+        classpath("com.android.tools.build:gradle:8.6.0") // Ensure you have the latest AGP version
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
